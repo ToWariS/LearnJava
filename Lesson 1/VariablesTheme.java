3aa289656df310e355f05a85fce804f9ee64631a
Sergei <ubaidula2@gmail.com>
2022-09-17 11:41:29 +0300

    Update VariablesTheme.java

--- Lesson 1/VariablesTheme.java
+++ Lesson 1/VariablesTheme.java
@@ -1,6 +1,5 @@
 public class VariablesTheme {
     public static void main(String[] args) {
-
         System.out.println("1. Создание переменных и вывод их значений на консоль");
 
         byte monitorCount = 2;
@@ -25,11 +24,9 @@ public class VariablesTheme {
         int bookPrice = 200;
         int penPrice = 100;
         int percentDiscount = 11;
-        int discount;
-        int discountPrice;
-        
-        discount = (penPrice + bookPrice) / penPrice * percentDiscount;
-        discountPrice = penPrice + bookPrice - discount;
+        int discount = (penPrice + bookPrice) / penPrice * percentDiscount;
+        int discountPrice = penPrice + bookPrice - discount;
+
         System.out.println("\nCумма скидки = " + discount + "\n"  +
                                     "Общая стоимость товаров со скидкой = " + discountPrice);
 
@@ -39,29 +36,25 @@ public class VariablesTheme {
                                     "J  J  aaaaa  V V  aaaaa\n" +
                                     " JJ  a     a  V  a     a");
 
-    System.out.println("\n4. Отображение min и max значений числовых типов данных\n");
+        System.out.println("\n4. Отображение min и max значений числовых типов данных\n");
 
-    byte byteMax ;
-    short shortMax ;
-    int intMax ;
-    long longMax ;
-     byteMax = Byte.MAX_VALUE;
-     shortMax = Short.MAX_VALUE;
-     intMax = Integer.MAX_VALUE;
-     longMax = Long.MAX_VALUE;
+        byte byteMax = Byte.MAX_VALUE;
+        short shortMax = Short.MAX_VALUE;
+        int intMax = Integer.MAX_VALUE;
+        long longMax = Long.MAX_VALUE;
 
-    System.out.println("Первоначальное значение:   "   + byteMax + "\t" + shortMax + "\t" + intMax + "\t" + longMax);
+        System.out.println("Первоначальное значение:   "   + byteMax + "\t" + shortMax + "\t" + intMax + "\t" + longMax);
 
-    System.out.println("Значение после инкремента: " + ++byteMax + "\t" + ++shortMax
-                                                     + "\t" + ++intMax + "\t" + ++longMax);
+        System.out.println("Значение после инкремента: " + ++byteMax + "\t" + ++shortMax
+                                    + "\t" + ++intMax + "\t" + ++longMax);
 
-    System.out.println("Значение после декремента: " + --byteMax + "\t" + --shortMax  + "\t"
-                                                     + --intMax + "\t" + --longMax);
+        System.out.println("Значение после декремента: " + --byteMax + "\t" + --shortMax  + "\t"
+                                    + --intMax + "\t" + --longMax);
 
-    System.out.println("\n5. Перестановка значений переменных\n");
-    int firstValue = 2;
-    int secondValue = 5;
-    int thirdValue;
+        System.out.println("\n5. Перестановка значений переменных\n");
+        int firstValue = 2;
+        int secondValue = 5;
+        int thirdValue;
 
         System.out.println("Перестановка значений переменных с помощью третьей переменной\n" +
                                     "Значения переменных: " + firstValue + "\t" + secondValue);
@@ -81,9 +74,9 @@ public class VariablesTheme {
         System.out.println("Перестановка значений переменных с помощью побитовой операции ^\n" +
                                     "Значения переменных: " + firstValue + "\t" + secondValue);
 
-        firstValue = firstValue ^ secondValue;
-        secondValue = secondValue ^ firstValue;
-        firstValue = firstValue ^ secondValue;
+        firstValue ^= secondValue;
+        secondValue ^= firstValue;
+        firstValue ^= secondValue;
 
         System.out.println("Значения переменных: " + firstValue + "\t" + secondValue);
 
@@ -95,9 +88,11 @@ public class VariablesTheme {
         char char94 = '^';
         char char95 = '_';
 
-        System.out.println(char35 + "\t" + (int) char35 + "\n" + char38 + "\t" + (int) char38 + "\n" +
-                           char64 + "\t" + (int) char64 + "\n" + char94 + "\t" + (int) char94 + "\n" +
-                           char95 + "\t" + (int) char95 + "\n");
+        System.out.println(char35 + "\t" + (int) char35 + "\n" +
+                                    char38 + "\t" + (int) char38 + "\n" +
+                                    char64 + "\t" + (int) char64 + "\n" +
+                                    char94 + "\t" + (int) char94 + "\n" +
+                                    char95 + "\t" + (int) char95 + "\n");
 
         System.out.println("\n7. Отображение количества сотен, десятков и единиц числа\n");
 
@@ -126,24 +121,19 @@ public class VariablesTheme {
 
         System.out.println("\n9. Произведение и сумма цифр числа\n");
 
-        int number;
-        int multNumbers;
-        int sumNumbers;
-        number = 345;
-        sumNumbers = (number / 100) + (number / 10 % 10) + (number % 10);
+        int number = 345;
+        int multNumbers = (number / 100) * (number / 10 % 10) * (number % 10);
+        int sumNumbers = (number / 100) + (number / 10 % 10) + (number % 10);
+
         System.out.println("Сумма цифр числа N = " + sumNumbers);
 
-        multNumbers = (number / 100) * (number / 10 % 10) * (number % 10);
         System.out.println("Произведение цифр числа N = " + multNumbers);
 
         System.out.println("\n10. Преобразование секунд\n");
         int secondCount = 86399;
-        int seconds;
-        int hour;
-        int min;
-        hour = secondCount / 3600;
-        min = (secondCount - hour * 3600) / 60;
-        seconds = secondCount  - hour * 3600 - min * 60;
+        int hour = secondCount / 3600;
+        int min = (secondCount - hour * 3600) / 60;
+        int seconds = secondCount  - hour * 3600 - min * 60;
 
         System.out.println(hour + ":" + min + ":" + seconds);
 
