57e82bf9cca2ad77a2482a67f901aaf49e9affae
Sergei <ubaidula2@gmail.com>
2022-09-19 12:11:27 +0300

    Update CyclesTheme.java

--- Lesson 1/CyclesTheme.java
+++ Lesson 1/CyclesTheme.java
@@ -6,7 +6,7 @@ public class CyclesTheme {
         int i = -10;
         int j = 0;
             do {
-                if(i % 2 == 0){
+                if(i % 2 == 0) {
                    sumEven += i;
                 } else {
                    sumUneven += i;
@@ -16,19 +16,19 @@ public class CyclesTheme {
         System.out.printf("В промежутке [-10, 21] сумма четных чисел = %d, а нечетных = %d\n" , sumEven, sumUneven);
 
         System.out.println("\n2. Вывод чисел в интервале (min и max) в порядке убывания\n");
-        int nums[] = { 10 , 5 , -1};
+        int nums[] = {10 , 5 , -1};
         int maxValue = 0;
         int minValue = 0;
-        for(i = 0 ; i < nums.length ; i++){
-            if( nums[i] > maxValue){
+        for(i = 0 ; i < nums.length ; i++) {
+            if( nums[i] > maxValue) {
                  maxValue = nums[i];
-            } else if(nums[i] < minValue){
+            } else if(nums[i] < minValue) {
                  minValue = nums[i];
             }
          }
 
         System.out.println("Интервал значений между " + minValue + " " + maxValue);
-        for (i = maxValue - 1 ; i > minValue ; i--){
+        for (i = maxValue - 1 ; i > minValue ; i--) {
              System.out.print(i + " ");
          }
 
@@ -46,7 +46,7 @@ public class CyclesTheme {
 
         System.out.println("\n4. Вывод чисел на консоль в несколько строк\n");
         int numCount = 0;
-        for( i = 1; i<24; i+=2){
+        for( i = 1; i<24; i+=2) {
             System.out.printf(" %d",i);
             numCount++;
             if(numCount == 5){
@@ -54,7 +54,7 @@ public class CyclesTheme {
                 numCount = 0;
             }
         }
-        while(numCount < 5){
+        while(numCount < 5) {
             System.out.printf(" %d",0);
             numCount++;
         }
@@ -66,14 +66,13 @@ public class CyclesTheme {
         int oneCount2 = 0;
         while(num1>0) {
             oneCount2 = num1 % 10;
-            if(oneCount2 == 1){
+            if(oneCount2 == 1) {
                 num1 += oneCount2;
             }
-
             num1 /= 10;
         }
 
-        if(num1 % 2 == 0){
+        if(num1 % 2 == 0) {
             System.out.printf("Число %d содержит %d четное количество единиц" , minValue , sumEven );
         } else {
             System.out.printf("Число %d содержит %d нечетное количество единиц" , minValue , sumEven );
@@ -81,8 +80,8 @@ public class CyclesTheme {
 
         System.out.println("\n\n6. Отображение фигур в консоли\n");
 
-        for (i = 0; i < 5; i++){
-            for (j = 0; j < 10; j++){
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 10; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -97,7 +96,6 @@ public class CyclesTheme {
                 System.out.print("#");
                 j++;
             }
-
             System.out.println();
             i--;
         }
@@ -130,12 +128,12 @@ public class CyclesTheme {
 
         System.out.println("\n\n7. Отображение ASCII-символов\n");
         char printChar = 0;
-        for(i = 0 ; i <= 122 ; i++){
+        for(i = 0 ; i <= 122 ; i++) {
             if(i <= 47 && i % 2 != 0) {
-                printChar = (char)i;
+                printChar = (char) i;
                 System.out.printf("%c %d\n" , printChar, i);
-            } else if(i >= 97 && i <= 122 && i % 2 == 0){
-                printChar = (char)i;
+            } else if(i >= 97 && i <= 122 && i % 2 == 0) {
+                printChar = (char) i;
                 System.out.printf("%c %d\n" , printChar, i);
             }
         }
@@ -145,7 +143,7 @@ public class CyclesTheme {
         num = 1234321;
         int sumFirst = num;
         int sumSecond = 0;
-        while( num != 0){
+        while( num != 0) {
             i = num % 10;
             sumSecond = sumSecond * 10 + i;
             num = num / 10;
@@ -163,17 +161,17 @@ public class CyclesTheme {
         sumSecond = num % 1000;
         int sum1 = 0;
         int sum2 = 0;
-        for ( ; sumFirst != 0; sumFirst /= 10 ){
+        for ( ; sumFirst != 0; sumFirst /= 10 ) {
             sum1 += sumFirst % 10;
         }
 
         System.out.printf("Сумма цифр %d = %d\n" , num / 1000 ,sum1);
-        for ( ; sumSecond != 0; sumSecond /= 10 ){
+        for ( ; sumSecond != 0; sumSecond /= 10 ) {
             sum2 += sumSecond % 10;
         }
 
         System.out.printf("Сумма цифр %d = %d\n" , num % 1000 ,sum2);
-        if(sum1 == sum2){
+        if(sum1 == sum2) {
             System.out.println("Число " + num + " счастливое!" );
         } else {
             System.out.println("Число " + num + " не счастливое!" );
@@ -181,21 +179,21 @@ public class CyclesTheme {
 
         System.out.println("\n\n10. Вывод таблицы умножения Пифагора\n");
         System.out.print(" |");
-        for (i = 2 ; i <= 9 ; i++){
+        for (i = 2 ; i <= 9 ; i++) {
             System.out.print(" " + i);
         }
         System.out.println();
         System.out.print(" -");
-        for (i = 2 ; i <= 9 ; i++){
+        for (i = 2 ; i <= 9 ; i++) {
             System.out.print(" -");
         }
         System.out.println();
-        for (i = 2 ; i <= 9 ; i++){
+        for (i = 2 ; i <= 9 ; i++) {
             System.out.print(i + "|");
-            for (j = 2 ; j <= 9 ; j++){
+            for (j = 2 ; j <= 9 ; j++) {
                 System.out.print(i * j + " ");
             }
             System.out.println();
         }
     }
-    }
+}
